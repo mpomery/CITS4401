25ebdfe0606a978eeafce46202f1d880b0f26595
Mitchell Pomery <bob_george33@hotmail.com>
2015-04-26 17:47:29 +0800

    Serves up files, but not if you try to make an API call

--- spacs/WebServer.py
+++ spacs/WebServer.py
@@ -1,6 +1,7 @@
 import BaseHTTPServer
 import LogHandler
 import string
+import os
 
 def start():
     PORT = 8000
@@ -36,15 +37,10 @@ class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         if string.lower(self.path).startswith("/api/"):
             LogHandler.log_warning("API Request")
+            self.handle_api_call()
         else:
             LogHandler.log_warning("Web Request")
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        #self.wfile.write("<html><head><title>Title goes here.</title></head>")
-        self.wfile.write("<body>")
-        self.wfile.write("<p>You accessed path: %s</p>" % self.path)
-        self.wfile.write("</body>")
+            self.handle_web_call()
     
     """
     Log that a request has been made. Occurs after headers are sent back.
@@ -55,3 +51,47 @@ class WebServer(BaseHTTPServer.BaseHTTPRequestHandler):
         message += " " + str(code)
         LogHandler.log_warning(message)
     
+    """
+    Handles all calls to the API
+    """
+    def handle_api_call(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("<html><head><title>API</title></head>")
+        self.wfile.write("<body>")
+        self.wfile.write("%s" % self.path)
+        self.wfile.write("</body>")
+    
+    """
+    Handles all calls to the main web server
+    """
+    def handle_web_call(self):
+        filetoserve = string.lower("www" + self.path)
+        # Check for missing index.html
+        if filetoserve[-1] == "/":
+            filetoserve = filetoserve + "index.html"
+        
+        # Check the file exists
+        if os.path.isfile(filetoserve):
+            self.send_response(200)
+        else:
+            filetoserve = "WWW/errors/404.html"
+            self.send_response(404)
+        
+        # Send Mime Type
+        if filetoserve.endswith(".html"):
+            self.send_header("Content-type", "text/html")
+        elif filetoserve.endswith(".js"):
+            self.send_header("Content-type", "application/javascript")
+        elif filetoserve.endswith(".css"):
+            self.send_header("Content-type", "text/css")
+        else:
+            self.send_header("Content-type", "application/octet-stream")
+        self.end_headers()
+        
+        # Forward them the file
+        f = open(filetoserve, 'r')
+        for line in f:
+            self.wfile.write(line)
+
