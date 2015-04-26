a17a30a4089a845694c797dedd58ff475b06afae
Mitchell Pomery <bob_george33@hotmail.com>
2015-04-26 20:22:06 +0800

    Add blank validators

--- spacs/src/Validator.py
+++ spacs/src/Validator.py
@@ -6,3 +6,10 @@ Returns true of input is an email address
 def is_email(email):
     regex = re.compile("^.*@.*\..*")
     return bool(regex.match(email))
+
+def is_phone_number(number):
+    return True
+
+def is_address(number):
+    return True
+
