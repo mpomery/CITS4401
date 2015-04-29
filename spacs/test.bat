3d46567641dbff3ae7992a47597687aee0b13dc2
Mitchell Pomery <bob_george33@hotmail.com>
2015-04-29 13:39:23 +0800

    Test Script
    Clears screen to make output easier to read

---
+++ spacs/test.bat
@@ -0,0 +1,13 @@
+cls
+@echo off
+rem Bypass "Terminate Batch Job" prompt.
+if "%~1"=="-FIXED_CTRL_C" (
+   REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+) ELSE (
+   REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :EOF
+)
+
+python make.py test
