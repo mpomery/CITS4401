37f98173e4cf3917791a6f9d04631fc7738c0821
Mitchell Pomery <bob_george33@hotmail.com>
2015-04-30 21:36:26 +0800

    Add user edit functionality

--- spacs/src/API.py
+++ spacs/src/API.py
@@ -102,3 +102,42 @@ Adds a Pool Owner to the system
 def po_add(data, parameter):
     return user_add(data, parameter, AuthUser.CreatePoolOwner())
 
+"""
+Edit a USER in the system
+"""
+def user_edit(data, parameter, au):
+    for property in TransactionBean.UserBean.properties:
+        try:
+            print("Setting UserBean" + str(property) + " To " + data["user"][str(property)])
+            setattr(au.user_info, str(property), data["user"][str(property)])
+        except ValueError, ve:
+            LogHandler.log_warning("Value Error: " + str(ve[0]))
+    
+    for property in TransactionBean.AuthenticationBean.properties:
+        try:
+            print("Setting AuthenticationBean" + str(property) + " To " + data["auth"][str(property)])
+            setattr(au.auth_info, str(property), data["auth"][str(property)])
+        except ValueError, ve:
+            LogHandler.log_warning("Value Error: " + str(ve[0]))
+    au.save()
+    id = au.id
+    return {"error": "success", "id": id}
+
+"""
+Edits an Administrator in the system
+"""
+def administrator_edit(data, parameter):
+    return user_edit(data, parameter, AuthUser.EditAdministrator(data["user"]["id"]))
+
+"""
+Edits a Pool Shop Admin in the system
+"""
+def psa_edit(data, parameter):
+    return user_edit(data, parameter, AuthUser.EditPoolShopAdmin(data["user"]["id"]))
+
+"""
+Edits a Pool Shop Owner in the system
+"""
+def po_edit(data, parameter):
+    return user_edit(data, parameter, AuthUser.EditPoolOwner(data["user"]["id"]))
+
